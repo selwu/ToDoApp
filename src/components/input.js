6fb9f8cf4669b565fd8ec441fa09660607d10911
selwu <selwu@mail.ru>
2020-08-26 14:03:18 +0300

    add html

---
+++ src/components/input.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './input.css';
+
+export default () => {
+  return <input type="text" className="todo-input form-control"/>;
+};
--- src/components/list-item.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-export default ({label, important = false}) => {
-  const style = {
-    color: important ? 'tomato' : 'black'
-  };
-  return <span style={style}>{label}</span>;
-};
