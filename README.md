a5d34d5ae611a4d38a937363bb19b00cbc63eed3
Alexander Dolgikh <sashafrontend@yandex.ru>
2021-02-02 23:29:53 +0300

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-
-# todo aplication
-
-## тренировка React
--- src/components/input/input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './input.css';
-
-const Input = () => {
-  return <input type="text" className="todo-input form-control" />;
-};
-
-export default Input;
